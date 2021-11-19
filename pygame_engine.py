552dd2870eeb46415bf10e4acf94e57ec21dbf19
Olle <olle.lapidus@elev.ga.ntig.se>
2021-11-19 08:25:34 +0100

    fixed fullscreen w/ magic

--- pygame_engine.py
+++ pygame_engine.py
@@ -1,9 +1,29 @@
 import pygame as pg
+import os
+import ctypes
+from ctypes import windll
 
-WIDTH, HEIGHT = 600, 600
+
+# os.environ['SDL_VIDEO_CENTERED'] = '1' # You have to call this before pygame.init()
 
 pg.init()
 
-screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+
+ctypes.windll.user32.SetProcessDPIAware()
+true_res = (windll.user32.GetSystemMetrics(0),windll.user32.GetSystemMetrics(1))
+WIDTH, HEIGHT = true_res
+# pygame.display.set_mode(true_res,pygame.Fullscreen)
+#
+# info = pg.display.Info() # You have to call this before pygame.display.set_mode()
+#
+# WIDTH, HEIGHT = info.current_w,info.current_h
+# WIDTH, HEGIHT = 1200, 800
+# print(WIDTH, HEIGHT)
+#
+# size = pg.display.list_modes()[0]
+# WIDTH, HEIGHT = size
+
+screen = pg.display.set_mode((WIDTH, HEIGHT), pg.FULLSCREEN)
 
 clock = pg.time.Clock()
